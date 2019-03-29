0efb81c05b199db086aab64c2a6c2bbe978cc7bd
XxVapeNationxX <45363501+XxVapeNationxX@users.noreply.github.com>
2019-03-29 15:42:54 -0400

    Add files via upload

---
+++ TP2 ASP.NET/TP2 ASP.NET/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TP2_ASP.NET
+{
+    public partial class Form1 : Form
+    {
+        public User Joueur1;
+        public User Joueur2;
+        public User Joueur3;
+        public User Joueur4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            User1.Text = Joueur1.Prenom;
+            User2.Text = Joueur2.Prenom;
+            if(Joueur3.Id != -1)
+            {
+                User3.Text = Joueur3.Prenom;
+                if(Joueur4.Id != -1)
+                {
+                    User4.Text = Joueur4.Prenom;
+                }
+            }
+            else
+            {
+                User3.Text = "";
+                User4.Text = "";
+            }
+        }
+
+        private void FinDeTour()
+        {
+            //Gérer la flèche
+            if (Fleche1.Enabled == true)
+            {
+                Fleche1.Enabled = false;
+                Fleche2.Enabled = true;
+            }
+            else if(Fleche2.Enabled == true)
+            {
+                Fleche2.Enabled = false;
+                Fleche3.Enabled = true;
+            }
+            else if (Fleche3.Enabled == true)
+            {
+                Fleche3.Enabled = false;
+                Fleche4.Enabled = true;
+            }
+            else if (Fleche4.Enabled == true)
+            {
+                Fleche4.Enabled = false;
+                Fleche1.Enabled = true;
+            }
+        }
+
+        private void PartieGagné()
+        {
+
+        }
+
+    }
+}
